69ffbd6c108f15e36151f3a02501765727b739e8
owent <owt5008137@live.com>
2017-02-15 12:45:49 +0800

    proto v3 support

--- src/register.c
+++ src/register.c
@@ -129,14 +129,43 @@ _set_default(struct _stringpool *pool, struct _field *f , int ptype, const char 
 
 static void
 _register_field(struct pbc_rmessage * field, struct _field * f, struct _stringpool *pool) {
+	int origin_label;
 	f->id = pbc_rmessage_integer(field, "number", 0 , 0);
 	f->type = pbc_rmessage_integer(field, "type", 0 , 0);	// enum
-	f->label = pbc_rmessage_integer(field, "label", 0, 0) - 1; // LABEL_OPTIONAL = 0
+	origin_label = pbc_rmessage_integer(field, "label", 0, 0) - 1; // LABEL_OPTIONAL = 0
+	f->label = origin_label;
+	switch(f->type) {
+	case PTYPE_DOUBLE:
+	case PTYPE_FLOAT:
+	case PTYPE_INT64:
+	case PTYPE_SINT64:  
+	case PTYPE_INT32:
+	case PTYPE_SINT32:  
+	case PTYPE_UINT32:
+	case PTYPE_ENUM:
+	case PTYPE_UINT64:
+	case PTYPE_FIXED32:
+	case PTYPE_SFIXED32:
+	case PTYPE_SFIXED64:
+	case PTYPE_FIXED64:
+	case PTYPE_BOOL:
+		if (f->label == LABEL_REPEATED) {
+			f->label = LABEL_PACKED;
+		}
+		break;
+    default:
+        break;
+    }
+	
 	if (pbc_rmessage_size(field , "options") > 0) {
 		struct pbc_rmessage * options = pbc_rmessage_message(field, "options" , 0);
 		int packed = pbc_rmessage_integer(options , "packed" , 0 , NULL);
+		// FIXME: Because pbc has default value for any field, packed attribute should be explicit set in .proto
+		//        if there is any options is set. Or pbc will set it to false and we will use unpacked encoding
 		if (packed) {
 			f->label = LABEL_PACKED;
+		} else {
+			f->label = origin_label;
 		}
 	}
 	f->type_name.n = pbc_rmessage_string(field, "type_name", 0 , NULL) +1;	// abandon prefix '.' 
